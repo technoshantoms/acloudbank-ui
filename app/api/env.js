bf530764f860683f2d6a6881096a00c118121bdc
Dennis K Satia <118351475+technoshantoms@users.noreply.github.com>
2025-08-29 15:50:26 +0300

    env

--- app/api/env.js
+++ app/api/env.js
@@ -2,24 +2,24 @@ const environment = process.env.NODE_ENV;
 
 let EES_BASE_URL;
 let REGISTRATION_SERVICE_BASE_URL;
-let RECAPTCHA_KEY;
+//let RECAPTCHA_KEY;
 let DEFAULT_WS_NODE;
 let WS_NODE_LIST_URL_NODE1;
 let WS_NODE_LIST_URL_NODE2;
 let WS_NODE_LIST_URL_NODE3;
 
 if (environment === "development") {
-    EES_BASE_URL = "http://localhost:3000";
-    REGISTRATION_SERVICE_BASE_URL = "http://localhost:48887";
-    // RECAPTCHA_KEY = "6Ldp0XQpAAAAAPkfUN6OUp4efAOyb3bnUy5ZC1yd";
+    EES_BASE_URL = "https://agent.acloudbank.com";
+    REGISTRATION_SERVICE_BASE_URL = "https://registra.acloudbank.com";
+     //RECAPTCHA_KEY = "6LdfqrIrAAAAAEi66M3VxRrGGgFkDBM9Ld6h4R2K";
     DEFAULT_WS_NODE = "wss://wss.acloudbank.com";
     WS_NODE_LIST_URL_NODE1 = "wss://wss.acloudbank.com";
     WS_NODE_LIST_URL_NODE2 = "wss://wss.acloudbank.com";
     WS_NODE_LIST_URL_NODE3 = "wss://wss.acloudbank.com";
 } else if (environment === "production") {
-    EES_BASE_URL = "http://localhost:3000";
-    REGISTRATION_SERVICE_BASE_URL = "http://localhost:48887";
-    // RECAPTCHA_KEY = "6Ldp0XQpAAAAAPkfUN6OUp4efAOyb3bnUy5ZC1yd";
+    EES_BASE_URL = "https://agent.acloudbank.com";
+    REGISTRATION_SERVICE_BASE_URL = "https://registra.acloudbank.com";
+     //RECAPTCHA_KEY = "6LdfqrIrAAAAAEi66M3VxRrGGgFkDBM9Ld6h4R2K";
     DEFAULT_WS_NODE = "wss://wss.acloudbank.com";
     WS_NODE_LIST_URL_NODE1 = "wss://wss.acloudbank.com";
     WS_NODE_LIST_URL_NODE2 = "wss://wss.acloudbank.com";
@@ -29,9 +29,10 @@ if (environment === "development") {
 export {
     EES_BASE_URL,
     REGISTRATION_SERVICE_BASE_URL,
-    // RECAPTCHA_KEY,
+     //RECAPTCHA_KEY,
     DEFAULT_WS_NODE,
     WS_NODE_LIST_URL_NODE1,
     WS_NODE_LIST_URL_NODE2,
     WS_NODE_LIST_URL_NODE3
 };
+
